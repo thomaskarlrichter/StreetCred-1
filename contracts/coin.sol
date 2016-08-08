34d1b52295448797651d21b632ab17aad2027a24
Kelvin Fichter <kef50@case.edu>
2016-08-08 16:28:27 -0400

    Added a few more contracts

--- contracts/coin.sol
+++ contracts/coin.sol
@@ -1,24 +1,12 @@
 contract StreetCred {
-    struct benefit {
-        bool can_receive;
-        uint balance;
-    }
+    mapping (address => uint) balances;
     
-    mapping (address => uint) public balances;
-    mapping (address => mapping (uint => benefit)) public benefits;
+    function StreetCred() {
+        
+    }
     
     modifier only_with_at_least(uint amount) {
         if (balances[msg.sender] < amount) throw;
-        _
-    }
-    
-    modifier only_with_at_least_benefits(uint benefit_id, uint amount) {
-        if (benefits[msg.sender][benefit_id].balance < amount) throw;
-        _
-    }
-    
-    modifier only_if_can_benefit(address receiver, uint benefit_id) {
-        if (!benefits[receiver][benefit_id].can_receive) throw;
         _
     }
     
@@ -28,11 +16,118 @@ contract StreetCred {
             balances[receiver] += amount;
         }
     }
+}
+
+contract User {
+    function setCred(address streetCredAddress);
+}
+
+contract Angel is User {
+    StreetCred cred;
+    //TODO: Each Angel needs a list of acceptors
+    
+    function Angel {
+        
+    }
+    
+    function setCred(address streetCredAddress) {
+        cred = StreetCred(streetCredAddress);
+    }
+}
+
+contract Acceptor is User {
+    struct benefit {
+        bool can_accept;
+        uint balance;
+    }
+    
+    StreetCred cred;
+    mapping (uint => benefit) benefits;
+    
+    function Acceptor() {
+        
+    }
+    
+    modifier only_if_can_accept(benefit_id) { // TODO: Make sure this acceptor is part of the beneficiary's angel's acceptors
+        if (!benefits[benefit_id].can_accept) throw;
+        _
+    }
+    
+    function setCred(address streetCredAddress) {
+        cred = StreetCred(streetCredAddress);
+    }
+    
+    // however we want to let acceptors get cash
+    function exchange() {
+        
+    }
+}
+
+contract Beneficiary is User {
+    struct benefit {
+        bool can_receive;
+        uint balance;
+    }
+    
+    StreetCred cred;
+    Angel angel;
+    mapping (uint => benefit) benefits;
+    
+    function Beneficiary() {
+        
+    }
+    
+    modifier only_if_can_benefit(uint benefit_id) {
+        if (!benefits[benefit_id].can_receive) throw;
+        _
+    }
+    
+    modifier only_with_at_least_benefits(uint benefit_id, uint amount) {
+        if (benefits[benefit_id].balance < amount) throw;
+        _
+    }
+    
+    function setCred(address streetCredAddress) {
+        cred = StreetCred(streetCredAddress);
+    }
+    
+    function setAngel(address angelAddress) {
+        angel = Angel(angelAddress);
+    }
+    
+    function spend_benefit(address receiver, uint benefit_id, uint amount) {
+        Acceptor acceptor = Acceptor(receiver);
+        only_with_at_least_benefits(benefit_id, amount) {
+            acceptor.only_if_can_accept(benefit_id) {
+                cred.send(acceptor, amount);
+                benefits[benefit_id].balance -= amount;
+                acceptor.benefits[benefit_id].balance += amount;
+            }
+        }
+    }
+}
+
+contract Donor is User {
+    StreetCred cred;
+    
+    function Donor() {
+        
+    }
+    
+    function setCred(address streetCredAddress) {
+        cred = StreetCred(streetCredAddress);
+    }
     
     function donate(address receiver, uint benefit_id, uint amount) {
-        only_if_can_benefit(receiver, benefit_id) {
-            send(receiver, amount);
-            benefits[receiver][benefit_id].balance += amount;
+        Beneficiary beneficiary = Beneficiary(receiver);
+        beneficiary.only_if_can_benefit(benefit_id) {
+            cred.send(receiver, amount);
+            beneficiary.benefits[benefit_id].balance += amount;
         }
+    }
+    
+    // however we want to implement this
+    function topup() {
+        
     }
 }
